09833c63c68cdba55cb8fadf2d9d2d135e95daab
AugustoOspino <augustoosrios@gmail.com>
2024-10-29 23:56:55 -0500

    Commit  edit2

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,2 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-const btn = document.getElementById('button');
-
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'default_service';
-   const templateID = 'template_udns3sj';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
